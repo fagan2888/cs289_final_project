1983bac74ed8732a651e56843fcb213cde3bc29f
Kevin Johnson <vadskye@gmail.com>
2014-04-28 00:59:30 -0700

    More numpy optimization

--- decision_nodes.py
+++ decision_nodes.py
@@ -14,6 +14,7 @@ class Node(object):
         self.splitting_threshold = None
         self.most_common_label = None
         self.impurity = None
+        self.label_sum = None
 
     def get_crashes(self):
         return Node.crashes[self.mi]
@@ -27,6 +28,12 @@ class Node(object):
         if self.impurity is None:
             self.impurity = calc_impurity(self.get_labels())
         return self.impurity
+
+    #Useful for split_on_feature_spam_only
+    def get_label_sum(self):
+        if self.label_sum is None:
+            self.label_sum = np.sum(self.get_labels())
+        return self.label_sum
 
     #Return the most common label among the values in this node
     #and its subnodes
@@ -56,28 +63,32 @@ class Node(object):
         return self.right.predicted_label(crash)
 
     def split(self, feature_consideration_limit):
+        #It is better to create these here rather than to make them an
+        # attribute of the node in order to limit the amount that must
+        # be kept these memory
         crashes = self.get_crashes()
+        crashes_by_feature = np.rollaxis(crashes, 1)
         labels = self.get_labels()
         #Limit the features we consider to generate more random trees
-        split_thresholds = self.calc_split_thresholds(crashes, labels,
+        split_thresholds = self.calc_split_thresholds(crashes_by_feature, labels,
                 feature_consideration_limit)
         splitting_feature, splitting_threshold, split_type = self._calc_best_split(
-                split_thresholds, crashes, labels)
+                split_thresholds, crashes_by_feature, labels)
         #if the node could not be split 
         if splitting_feature == -1:
             return None, None, None, None, None
         left_node_indexes, right_node_indexes = self.split_on_feature(splitting_feature, splitting_threshold, crashes)
         return left_node_indexes, right_node_indexes, splitting_feature, splitting_threshold, split_type
 
-    def _calc_best_split(self, split_thresholds, crashes, labels):
+    def _calc_best_split(self, split_thresholds, crashes_by_feature, labels):
         current_impurity = self.get_impurity()
         best_impurity_drop = 0
         best_feature_index = -1
         best_split_value = 0
         best_split_type = -1
         #for each feature
-        crashes_by_feature = np.rollaxis(crashes, 1)
         for i, split_thresholds_i in enumerate(split_thresholds):
+            #For each threshold value used to split the node
             for j, split_value in enumerate(split_thresholds_i):
                 left_spam, left_size, right_spam, right_size = self.split_on_feature_spam_only(i, split_value, crashes_by_feature[i], labels)
                 impurity_drop = calc_impurity_drop_spam_only(current_impurity, left_spam, left_size, right_spam, right_size)
@@ -89,46 +100,52 @@ class Node(object):
                     best_split_type = j
         return best_feature_index, best_split_value, best_split_type
 
-    def calc_split_thresholds(self, crashes, labels,
+    def calc_split_thresholds(self, crashes_by_feature, labels,
             feature_consideration_limit, search_type='edge'):
         if feature_consideration_limit is None:
             features_to_consider = xrange(Node.feature_count)
         else:
             features_to_consider = util.randsample(Node.feature_count, 
                     feature_consideration_limit)
-        split_thresholds = list()
+        threshold_count = {
+                'edge': 5,
+                'fixed': 9,
+                'mean': 1,
+                'median': 1,
+                }[search_type]
+        split_thresholds = np.empty((Node.feature_count, threshold_count))
+        #split_thresholds = list()
         #f represents which feature we're evaluating
         #Normally, f ranges from 0 to 57
         for f in features_to_consider:
-            feature_f = [crash[f] for crash in crashes]
             if search_type == 'edge':
                 #The set of features from each crash with that label
-                spam_f = list()
-                nonspam_f = list()
-                #i is which crash we're looking at
-                for i, feature in enumerate(feature_f):
-                    if labels[i]:
-                        spam_f.append(feature)
-                    else:
-                        nonspam_f.append(feature)
-                split_thresholds.append([mina(spam_f), maxa(spam_f),
-                    mina(nonspam_f), maxa(nonspam_f), util.avg(feature_f)])
+                #spam_f = list()
+                #nonspam_f = list()
+                spam_crash_indexes = labels==1
+                spam_f = crashes_by_feature[f][spam_crash_indexes]
+                nonspam_f = crashes_by_feature[f][~spam_crash_indexes]
+                split_thresholds[f] = np.array([mina(spam_f), maxa(spam_f),
+                    mina(nonspam_f), maxa(nonspam_f),
+                    np.mean(crashes_by_feature[f])])
 
             elif search_type=='fixed':
-                split_thresholds.append(np.linspace(min(feature_f), 
-                    max(feature_f), 9))
+                split_thresholds[f] = np.array(
+                        np.linspace(min(crashes_by_feature[f]),
+                        max(crashes_by_feature[f]), threshold_count['fixed']))
                     
             elif search_type=='mean':
-                average = util.avg(feature_f)
+                average = np.mean(crashes_by_feature[f])
                 #minimum never helps for some reason
-                minimum = min(feature_f)
-                maximum = max(feature_f)
-                avg_low = util.avg([average, minimum])
-                avg_high = util.avg([average, maximum])
-                split_thresholds.append([average])
+                minimum = min(crashes_by_feature[f])
+                maximum = max(crashes_by_feature[f])
+                avg_low = np.mean([average, minimum])
+                avg_high = np.mean([average, maximum])
+                split_thresholds[f] = np.array([average])
             elif search_type=='median':
-                feature_f.sort()
-                split_thresholds.append([get_median_of_sorted(feature_f)])
+                crashes_by_feature[f].sort()
+                split_thresholds[f] = np.array([get_median_of_sorted(
+                    crashes_by_feature[f])])
             else:
                 print 'invalid average type', search_type
                 return False
@@ -142,7 +159,7 @@ class Node(object):
         #print threshold, len(crashes_by_feature)
         labels_thresholded = labels[crashes_by_feature>=threshold]
         #print len(labels_thresholded)
-        total = np.sum(labels)
+        total = self.get_label_sum()
         left_spam = np.sum(labels_thresholded)
         right_spam = total - left_spam
         left_size = len(labels_thresholded)
