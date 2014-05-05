1629c539d6287252d7c63617b7a64c877eb57f50
Kevin Johnson <vadskye@gmail.com>
2014-05-04 21:53:54 -0700

    standardize to mean 0

--- logistic_regression.py
+++ logistic_regression.py
@@ -272,7 +272,8 @@ def calc_labels(x, beta):
 
 def assign_labels(x_train, y_train, lam, step_size,
         iterations, k, weight_step=True, x_test = None):
-    x_train = log_transform_data(x_train)
+    #x_train = log_transform_data(x_train)
+    x_train = standardize_data(x_train)
 
     beta = cross_validate(x_train, y_train, lam, step_size, iterations,
             weight_step, k)
@@ -283,9 +284,9 @@ def assign_labels(x_train, y_train, lam, step_size,
 
     if x_test is not None:
         #Now work on the actual test data
-        outputfile = open('kaggle_submission_test.txt', 'w')
+        outputfile = open('x_test.txt', 'w')
         outputfile.write('Id,Category\n')
-        x_test = log_transform_data(x_test)
+        x_test = standardize_data(x_test)
 
         labels_calc_test = calc_labels(x_test, beta)
         for i, label in enumerate(labels_calc_test):
