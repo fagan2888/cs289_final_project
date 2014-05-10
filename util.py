146de6a716b2178f4173bc2f8a6284a947aa17de
Kevin Johnson <vadskye@gmail.com>
2014-05-09 18:15:31 -0700

    Method to import 2011, 2012 data intelligently

--- util.py
+++ util.py
@@ -77,4 +77,32 @@ def import_cyclist_data(input_filename):
     #print df.info()
     x = df.values
     return x, y
+
+#Import the 2011 and 2012 data intelligently, stripping out columns from 2011
+#data that don't exist in 2012 data
+def smart_import_cyclist_data(input_filename_2011, input_filename_2012):
+    input_file_2011 = open(input_filename_2011, 'rb')
+    input_file_2012 = open(input_filename_2012, 'rb')
+    #pandas dataframe
+    data_2011 = cPickle.load(input_file_2011)
+    data_2012 = cPickle.load(input_file_2012)
+    print data_2011.shape, data_2012.shape
+    print type(data_2012.columns)
+    shared_columns = [col for col in data_2011.columns.values
+            if col in data_2012.columns.values]
+    print len(shared_columns)
+    #data_2011 = data_2011[data_2012.columns.values]
+    data_2011 = data_2011[shared_columns]
+    data_2012 = data_2012[shared_columns]
+    print data_2011.shape, data_2012.shape
+    x_train, y_train = extract_xy_from_cyclist_data(data_2011)
+    x_test, y_test = extract_xy_from_cyclist_data(data_2012)
+    return x_train, y_train, x_test, y_test
+
     
+def extract_xy_from_cyclist_data(cyclist_data):
+    y = cyclist_data['SER_INJ'].values
+    cyclist_data = cyclist_data.drop('SER_INJ', 1)
+    #print cyclist_data.info()
+    x = cyclist_data.values
+    return x, y
